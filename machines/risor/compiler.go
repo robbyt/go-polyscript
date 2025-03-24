effaddc804758f62ed831f20f5321cbc5b52d8b3
RT <robbyt@robbyt.net>
2025-03-23 22:28:55 -0400

    switch to helpers.SetupLogger, change some ordering/layout

--- machines/risor/compiler.go
+++ machines/risor/compiler.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
-	"os"
 	"strings"
 
 	"github.com/robbyt/go-polyscript/execution/script"
+	"github.com/robbyt/go-polyscript/internal/helpers"
 )
 
 type Compiler struct {
@@ -23,19 +23,36 @@ type CompilerOptions interface {
 // NewCompiler creates a new Risor-specific Compiler instance with the provided global variables.
 // Global variables are used for initial script parsing while building the executable bytecode.
 func NewCompiler(handler slog.Handler, compilerOptions CompilerOptions) *Compiler {
-	if handler == nil {
-		defaultHandler := slog.NewTextHandler(os.Stdout, nil)
-		handler = defaultHandler.WithGroup("risor")
-		// Create a logger from the handler rather than using slog directly
-		defaultLogger := slog.New(handler)
-		defaultLogger.Warn("Handler is nil, using the default logger configuration.")
-	}
+	handler, logger := helpers.SetupLogger(handler, "risor", "Compiler")
 
 	return &Compiler{
 		globals:    compilerOptions.GetGlobals(),
 		logHandler: handler,
-		logger:     slog.New(handler.WithGroup("Compiler")),
+		logger:     logger,
 	}
+}
+
+func (c *Compiler) String() string {
+	return "risor.Compiler"
+}
+
+// Compile turns the provided script content into runnable bytecode.
+func (c *Compiler) Compile(scriptLoader io.ReadCloser) (script.ExecutableContent, error) {
+	if scriptLoader == nil {
+		return nil, ErrContentNil
+	}
+
+	scriptBodyBytes, err := io.ReadAll(scriptLoader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read script: %w", err)
+	}
+
+	err = scriptLoader.Close()
+	if err != nil {
+		return nil, fmt.Errorf("failed to close reader: %w", err)
+	}
+
+	return c.compile(scriptBodyBytes)
 }
 
 func (c *Compiler) compile(scriptBodyBytes []byte) (*Executable, error) {
@@ -54,7 +71,7 @@ func (c *Compiler) compile(scriptBodyBytes []byte) (*Executable, error) {
 
 	// Check for comment-only script
 	isCommentOnly := true
-	for _, line := range strings.Split(trimmedScript, "\n") {
+	for line := range strings.SplitSeq(trimmedScript, "\n") {
 		if trimmedLine := strings.TrimSpace(line); trimmedLine != "" && !strings.HasPrefix(trimmedLine, "#") {
 			// Found a non-comment line, so we can stop checking lines because there's some real code here!
 			isCommentOnly = false
@@ -94,27 +111,4 @@ func (c *Compiler) compile(scriptBodyBytes []byte) (*Executable, error) {
 
 	logger.Debug("Validation completed")
 	return risorExec, nil
-}
-
-func (c *Compiler) String() string {
-	return "risor.Compiler"
-}
-
-// Compile turns the provided script content into runnable bytecode.
-func (c *Compiler) Compile(scriptReader io.ReadCloser) (script.ExecutableContent, error) {
-	if scriptReader == nil {
-		return nil, ErrContentNil
-	}
-
-	scriptBodyBytes, err := io.ReadAll(scriptReader)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read script: %w", err)
-	}
-
-	err = scriptReader.Close()
-	if err != nil {
-		return nil, fmt.Errorf("failed to close reader: %w", err)
-	}
-
-	return c.compile(scriptBodyBytes)
 }
